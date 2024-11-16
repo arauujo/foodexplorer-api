d9eba717fcf213508cdb5b3f5821f480a45d8388
arauujo <wesleey988@gmail.com>
2024-11-16 16:50:59 -0300

    Add error handling middleware to server

--- src/server.js
+++ src/server.js
@@ -8,5 +8,12 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ error: "Something went wrong!", details: err.message });
+});
+
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
