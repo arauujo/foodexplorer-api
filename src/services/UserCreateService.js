6249e91ce4f7b4f1b6bae4536c3f6ed2ff6157b8
arauujo <wesleey988@gmail.com>
2024-11-23 14:09:44 -0300

    Add validation for user creation fields in UserCreateService

--- src/services/UserCreateService.js
+++ src/services/UserCreateService.js
@@ -7,11 +7,13 @@ class UserCreateService {
   }
 
   async execute({ name, email, password }) {
+    if (!name || !email || !password) {
+      throw new AppError("É necessário preencher todos os campos.");
+    }
     const userExists = await this.userRepository.findByEmail(email);
     if (userExists) {
       throw new AppError("Este e-mail já está em uso.");
     }
-
     const hashedPassword = await hash(password, 8);
     await this.userRepository.create({ name, email, password: hashedPassword });
   }
